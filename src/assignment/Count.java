00879c69ac8ebb8d12a04f35db3a00e9f7f65f7b
dcheryasov <dmitry.cheryasov@gmail.com>
2016-10-11 08:18:55 -0400

    WIP: Solving the correct problem; much useless code thrown away,

--- src/assignment/Count.java
+++ src/assignment/Count.java
@@ -6,26 +6,32 @@ import java.util.Map;
 
 /**
  * Counts the number of times a particular word occurred in input.
+ * A hash table has an efficient representation of a key set, easy to check for presence of query words.
  */
-class Counter {
+class Count {
     // NOTE: if we ran counting in parallel, we'd have a ConcurrentHashMap<String, AtomicInteger>.
+    // If we user a tree-based map instead, we could cheaply return new immutable instances on every .count()
+    // invocation. But we never need intermediate results here. The lookup time would be somehow worse, too.
+
     private final HashMap<String, Integer> myWordCount;
 
-    Counter() {
-        this.myWordCount = new HashMap<String, Integer>();
-    }
+    Count() { this.myWordCount = new HashMap<>(); }
 
-    public void count(String word) {
+    /**
+     * Adds / updates the count for the {@code word} by {@code amount}.
+     * */
+    void count(String word) {
         int count_so_far;
         if (myWordCount.containsKey(word)) {
             // We are sure not to have any nulls, so unboxing right away.
             count_so_far = myWordCount.get(word) + 1;
-        }
-        else count_so_far = 1;
+        } else count_so_far = 1;
         myWordCount.put(word, count_so_far);
     }
 
-    public Map<String, Integer> getCounts() {
+    Map<String, Integer> getCounts() {
         return Collections.unmodifiableMap(myWordCount);
     }
+
 }
+
--- src/assignment/Main.java
+++ src/assignment/Main.java
@@ -4,14 +4,20 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.Map;
-import java.util.function.BiConsumer;
-import java.util.function.Consumer;
+import java.util.*;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
-import java.util.stream.StreamSupport;
 
 /**
- * Command-line interface.
+ * The problem: <b/><i>
+ * Consider a list of sets of words (each set of words is called a record) and a single set of words (called the query).
+ * For each word that is not in the query, how many times does the word appear in all records that the entire query is
+ * present in? Output a dictionary of words to counts, omitting words with counts of zero. Given a list of records and
+ * a list of queries, determine the output for each query with respect to the entire list of records.
+ * </i>
+ * <p/>
+ * Command-line interface class.
  */
 public class Main {
 
@@ -29,18 +35,48 @@ public class Main {
         return builder.toString();
     }
 
+    static Stream<String> lineAsWordStream(String input) {
+        return Arrays.stream(input.split(","));
+    }
+
+    static boolean divulge(Object x) {
+        System.out.println(String.valueOf(x));
+        return true;
+    }
+
     public static void main(String[] args) {
-        if (args.length != 1) {
-            System.out.println("Usage: <prog> request_file_name");
+        if (args.length != 2) {
+            System.out.println("Usage: <prog> records_file query_file");
             System.exit(1);
         }
         // Produce the actual (I/O-error-prone) string stream from file.
+        final Path record_file_path = Paths.get(args[0]);
+        final Path query_file_path = Paths.get(args[1]);
         try {
-            Stream<String> input_lines = Files.lines(Paths.get(args[0]));
-            Stream<String> words = input_lines.flatMap(new LineParser());
-            Counter counter = new Counter();
-            words.forEach(counter::count);
-            System.out.println(jsonify(counter.getCounts()));
+            // We do not try to detect and report repeating words in data; we treat them as one.
+            Stream<String> record_lines = Files.lines(record_file_path);
+            Stream<Set<String>> records_set_stream = record_lines.map(Main::lineAsWordStream).map(st -> st.collect(Collectors.toSet()));;
+
+            // We cannot operate directly on the stream of records, because we have more than one query.
+            List<Set<String>> records = records_set_stream.collect(Collectors.toList());
+
+            // TODO: make a word -> [records] index to speed up scanning.
+
+            // Prepare the queries to run against the counts we have.
+            Stream<String> query_lines = Files.lines(query_file_path);
+            Stream<Set<String>> word_sets = query_lines.map(Main::lineAsWordStream)
+                    .map(st -> st.collect(Collectors.toSet()));
+
+            // Run each word set as a query against the counts.
+            word_sets.forEach(query_words -> {
+                System.out.println(query_words.toString()); // XXX
+                Count missing_words_count = new Count();
+                Stream<Set<String>> matching_records = records.stream().filter(record -> record.containsAll(query_words));
+                Stream<String> missing_words = matching_records.flatMap(Collection::stream).filter(word -> !query_words.contains(word));
+                missing_words.forEach(word -> missing_words_count.count(word));
+                System.out.println(jsonify(missing_words_count.getCounts()));
+            });
+
         } catch (IOException e) {
             e.printStackTrace(); // TODO: nicer handling.
         }
